e9d03c360c956f73d4312b87250272b4e606140b
임현수 <21port@naver.com>
2020-04-09 18:03:12 +0900

    update checkpoint

--- preprocess_controller.py
+++ preprocess_controller.py
@@ -13,6 +13,7 @@ import create
 import CNN
 import RNN
 import pre_trained_model
+
 # - 이미지 데이터 불러오기 링크
 # https://m.blog.naver.com/PostView.nhn?blogId=baek2sm&logNo=221400912923&categoryNo=36&proxyReferer=https%3A%2F%2Fwww.google.com%2F
 
@@ -29,13 +30,14 @@ img_paths, captions = preprocess.get_path_caption()
 img_name_vector, train_captions = img_preprocess.img_pre(img_paths, captions)
 # ===========================
 
-
 # ===========================================
 # 캡션 토큰화 데이터 받아오기,, 리턴은 토크나이저
 # my_tokenizer = None
 # if(my_token.Load_Tokenizer() == None):
 #     print("dddddddddddddddddddddd")
-tokenizer, cap_vector, train_seqs, max_length = my_token.tokenization(train_captions[:my_token.top_k*5])
+tokenizer, cap_vector, train_seqs, max_length = my_token.tokenization(
+    train_captions[: my_token.top_k * 5]
+)
 #     my_token.Save_Tokenizer(my_tokenizer)
 #     my_token.Save_Captions(cap_vector)
 #     my_token.Save_Seqs(train_seqs)
@@ -64,8 +66,10 @@ tokenizer, cap_vector, train_seqs, max_length = my_token.tokenization(train_capt
 # test_size가 0.2이면 train_size는 0.8
 # 따라서 train:test = 80% : 20%
 img_name_train, img_name_val, cap_train, cap_val = train_test_split(
-    img_name_vector, cap_vector, test_size=0.2, random_state=0)
+    img_name_vector, cap_vector, test_size=0.2, random_state=0
+)
 # ====================-=======
+
 
 # # tf 데이터셋 만들기
 dataset = create.create_tf_data(tokenizer, img_name_train, cap_train)
@@ -82,16 +86,7 @@ decoder = RNN.RNN_Decoder(embedding_dim, units, vocab_size)
 
 print(encoder, decoder)
 
-# image_features_extract_model = pre_trained_model.Pre_trained_img(img_name_vector)
-
-
-
-
-
-
-
-
-
+image_features_extract_model = pre_trained_model.Pre_trained_img(img_name_vector)
 
 
 
@@ -128,8 +123,6 @@ print(encoder, decoder)
 # print(len(img_name_train), len(cap_train), len(img_name_val), len(cap_val))
 
 
-
-
 class BahdanauAttention(tf.keras.Model):
     def __init__(self, units):
         super(BahdanauAttention, self).__init__()
@@ -157,9 +150,12 @@ class BahdanauAttention(tf.keras.Model):
 
         return context_vector, attention_weights
 
+
 optimizer = tf.keras.optimizers.Adam()
 loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
-    from_logits=True, reduction='none')
+    from_logits=True, reduction="none"
+)
+
 
 def loss_function(real, pred):
     mask = tf.math.logical_not(tf.math.equal(real, 0))
@@ -170,21 +166,21 @@ def loss_function(real, pred):
 
     return tf.reduce_mean(loss_)
 
+
 checkpoint_path = "./checkpoints/train"
-ckpt = tf.train.Checkpoint(encoder=encoder,
-                           decoder=decoder,
-                           optimizer = optimizer)
+ckpt = tf.train.Checkpoint(encoder=encoder, decoder=decoder, optimizer=optimizer)
 ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
 
 start_epoch = 0
-# if ckpt_manager.latest_checkpoint:
-#     start_epoch = int(ckpt_manager.latest_checkpoint.split('-')[-1])
-#     # restoring the latest checkpoint in checkpoint_path
-#     ckpt.restore(ckpt_manager.latest_checkpoint)
+if ckpt_manager.latest_checkpoint:
+    start_epoch = int(ckpt_manager.latest_checkpoint.split('-')[-1])
+    # restoring the latest checkpoint in checkpoint_path
+    ckpt.restore(ckpt_manager.latest_checkpoint)
 
 # adding this in a separate cell because if you run the training cell
 # many times, the loss_plot array will be reset
 loss_plot = []
+
 
 @tf.function
 def train_step(img_tensor, target):
@@ -194,7 +190,7 @@ def train_step(img_tensor, target):
     # because the captions are not related from image to image
     hidden = decoder.reset_state(batch_size=target.shape[0])
 
-    dec_input = tf.expand_dims([tokenizer.word_index['<start>']] * target.shape[0], 1)
+    dec_input = tf.expand_dims([tokenizer.word_index["<start>"]] * target.shape[0], 1)
 
     with tf.GradientTape() as tape:
         features = encoder(img_tensor)
@@ -208,7 +204,7 @@ def train_step(img_tensor, target):
             # using teacher forcing
             dec_input = tf.expand_dims(target[:, i], 1)
 
-    total_loss = (loss / int(target.shape[1]))
+    total_loss = loss / int(target.shape[1])
 
     trainable_variables = encoder.trainable_variables + decoder.trainable_variables
 
@@ -217,11 +213,13 @@ def train_step(img_tensor, target):
     optimizer.apply_gradients(zip(gradients, trainable_variables))
 
     return loss, total_loss
+
+
 print("학습을 시작합니다")
 BATCH_SIZE = 64
 BUFFER_SIZE = 1000
 
-EPOCHS = 5
+EPOCHS = 100
 num_steps = len(img_name_train) // BATCH_SIZE
 
 for epoch in range(start_epoch, EPOCHS):
@@ -233,31 +231,35 @@ for epoch in range(start_epoch, EPOCHS):
         total_loss += t_loss
 
         if batch % 100 == 0:
-            print ('Epoch {} Batch {} Loss {:.4f}'.format(
-                epoch + 1, batch, batch_loss.numpy() / int(target.shape[1])))
+            print(
+                "Epoch {} Batch {} Loss {:.4f}".format(
+                    epoch + 1, batch, batch_loss.numpy() / int(target.shape[1])
+                )
+            )
     # storing the epoch end loss value to plot later
     loss_plot.append(total_loss / num_steps)
 
     if epoch % 5 == 0:
         ckpt_manager.save()
 
-    print ('Epoch {} Loss {:.6f}'.format(epoch + 1,
-                                         total_loss/num_steps))
-    print ('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+    print("Epoch {} Loss {:.6f}".format(epoch + 1, total_loss / num_steps))
+    print("Time taken for 1 epoch {} sec\n".format(time.time() - start))
 
 plt.plot(loss_plot)
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.title('Loss Plot')
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.title("Loss Plot")
 plt.show()
 
+
 def calc_max_length(tensor):
-    print(tensor)
     return max(len(t) for t in tensor)
+
 
 max_length = calc_max_length(train_seqs)
 
 attention_features_shape = 64
+
 
 def evaluate(image):
     attention_plot = np.zeros((max_length, attention_features_shape))
@@ -266,28 +268,31 @@ def evaluate(image):
 
     temp_input = tf.expand_dims(pre_trained_model.load_image(image)[0], 0)
     img_tensor_val = image_features_extract_model(temp_input)
-    img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+    img_tensor_val = tf.reshape(
+        img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3])
+    )
 
     features = encoder(img_tensor_val)
 
-    dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
+    dec_input = tf.expand_dims([tokenizer.word_index["<start>"]], 0)
     result = []
 
     for i in range(max_length):
         predictions, hidden, attention_weights = decoder(dec_input, features, hidden)
 
-        attention_plot[i] = tf.reshape(attention_weights, (-1, )).numpy()
+        attention_plot[i] = tf.reshape(attention_weights, (-1,)).numpy()
 
         predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
         result.append(tokenizer.index_word[predicted_id])
 
-        if tokenizer.index_word[predicted_id] == '<end>':
+        if tokenizer.index_word[predicted_id] == "<end>":
             return result, attention_plot
 
         dec_input = tf.expand_dims([predicted_id], 0)
 
-    attention_plot = attention_plot[:len(result), :]
+    attention_plot = attention_plot[: len(result), :]
     return result, attention_plot
+
 
 def plot_attention(image, result, attention_plot):
     temp_image = np.array(Image.open(image))
@@ -297,22 +302,23 @@ def plot_attention(image, result, attention_plot):
     len_result = len(result)
     for l in range(len_result):
         temp_att = np.resize(attention_plot[l], (8, 8))
-        ax = fig.add_subplot(len_result//2, len_result//2, l+1)
+        ax = fig.add_subplot(len_result // 2, len_result // 2, l + 1)
         ax.set_title(result[l])
         img = ax.imshow(temp_image)
-        ax.imshow(temp_att, cmap='gray', alpha=0.6, extent=img.get_extent())
+        ax.imshow(temp_att, cmap="gray", alpha=0.6, extent=img.get_extent())
 
     plt.tight_layout()
     plt.show()
 
+
 # captions on the validation set
 rid = np.random.randint(0, len(img_name_val))
 image = img_name_val[rid]
-real_caption = ' '.join([tokenizer.index_word[i] for i in cap_val[rid] if i not in [0]])
+real_caption = " ".join([tokenizer.index_word[i] for i in cap_val[rid] if i not in [0]])
 result, attention_plot = evaluate(image)
 
-print ('Real Caption:', real_caption)
-print ('Prediction Caption:', ' '.join(result))
+print("Real Caption:", real_caption)
+print("Prediction Caption:", " ".join(result))
 plot_attention(image, result, attention_plot)
 
 # image_url = 'https://tensorflow.org/images/surf.jpg'
