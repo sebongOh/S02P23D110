370f5ea93051eb85219a2fb32ad3050b7f022f01
임현수 <21port@naver.com>
2020-04-17 16:32:13 +0900

    init front

---
+++ web/front/src/vuex/actions.js
@@ -0,0 +1,11 @@
+export default {
+  // checkPage(context) {
+  //     context.commit('setPageTitle');
+  // },
+  // login({ commit }, auth) {
+  //     commit('login', auth);
+  // },
+  // toggleNav(context) {
+  //     context.commit('toggleNav');
+  // }
+};
---
+++ web/front/src/vuex/getters.js
@@ -0,0 +1,3 @@
+export default {
+  // getNoticeNum: state => state.noticeNum
+};
