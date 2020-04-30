9f27d2fa5626c0d099f2332cc427fd2c0e2459b2
박찬환 <chanchanhwan@naver.com>
2020-04-30 17:47:39 +0900

    loading spinner

---
+++ web/frontend/src/components/LoadingBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-overlay opacity="0.8">
+
+  <div class="loading">로딩중 입니다..</div>
+
+  <v-progress-circular
+      :size="200"
+      :width="15"
+      color="white"
+      indeterminate
+    ></v-progress-circular>
+  
+  </v-overlay>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+ mounted() {
+
+ }
+}
+</script>
+
+<style lang="scss" scoped>
+  .loading{
+    font-size: 3rem;
+  }
+</style>
