185a86cfbbddfec90f2c24d482cb62398eeb9aff
Woogie924 <hsw924@gmail.com>
2020-05-01 02:43:42 +0900

    [UPDATE] detail page layout, 여러가지 css작업

--- web/frontend/src/components/CarImages.vue
+++ web/frontend/src/components/CarImages.vue
@@ -1,12 +1,19 @@
 <template>
   <v-layout wrap>
-    <v-flex v-for="item in aiItems" :key="item.id" lg4 sm6 md6 xs12>
+    <v-flex v-for="item in aiItems" :key="item.id" lg2 sm2 md2 xs12>
       <v-hover v-slot:default="{ hover }">
-        <v-card elevation="0" max-width="344" style="border: 1px solid;" class="ma-auto">
+        <v-card elevation="0" max-width="344" class="ma-auto">
           <v-img :src="item.imagelink" class="white--text" height="200px">
             <v-expand-transition>
-              <div v-if="hover" class="d-flex transition-fast-in-fast-out grey display-3 white--text justify-center" style="height: 100%;">
-                <v-card-title v-text="item.name" @click="getDetail(item.id)"></v-card-title>
+              <div
+                v-if="hover"
+                class="d-flex transition-fast-in-fast-out grey display-3 white--text justify-center"
+                style="height: 100%;"
+              >
+                <v-card-title
+                  v-text="item.name"
+                  @click="getDetail(item.id)"
+                ></v-card-title>
               </div>
             </v-expand-transition>
           </v-img>
