7ed91bf50b2334a96fb21863e4fe42ee00991651
박찬환 <chanchanhwan@naver.com>
2020-04-30 18:28:11 +0900

    loading spinner

--- web/frontend/src/components/LoadingBar.vue
+++ web/frontend/src/components/LoadingBar.vue
@@ -1,8 +1,23 @@
 <template>
   <v-overlay opacity="0.8">
 
-  <div class="loading">로딩중 입니다..</div>
-
+  <div>
+  <vue-typer class="loading"
+    :text='["로딩중 입니다...", "정보를 가져오는 중입니다..."]'
+    :repeat='Infinity'
+    :shuffle='false'
+    initial-action='typing'
+    :pre-type-delay='70'
+    :type-delay='100'
+    :pre-erase-delay='2000'
+    :erase-delay='250'
+    erase-style='clear'
+    :erase-on-complete='false'
+    caret-animation='smooth'
+></vue-typer>
+  </div>
+<br>
+<br>
   <v-progress-circular
       :size="200"
       :width="15"
@@ -14,9 +29,10 @@
 </template>
 
 <script>
-
+import { VueTyper } from 'vue-typer'
 export default {
   components: {
+    VueTyper,
   },
   data() {
     return {
@@ -28,8 +44,11 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .loading{
     font-size: 3rem;
+  }
+  .vue-typer .char{
+  color: white !important;
   }
 </style>
